f7d2e3c323322dc8ccf87dae5ce15e43eeee37a1
Julia Kornich <juliako@microsoft.com>
2020-10-26 19:44:20 -0700

    updated 'Region' field to 'Location'

--- AMSV3Quickstarts/EncodeAndStreamFiles/ConfigWrapper.cs
+++ AMSV3Quickstarts/EncodeAndStreamFiles/ConfigWrapper.cs
@@ -58,9 +58,9 @@ namespace EncodeAndStreamFiles
             get { return new Uri(_config["ArmEndpoint"]); }
         }
 
-        public string Region
+        public string Location
         {
-            get { return _config["Region"]; }
+            get { return _config["Location"]; }
         }
     }
 }
